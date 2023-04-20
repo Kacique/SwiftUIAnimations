6e85e0b2f6ea4c6c100041fb39a00e3329b62463
Kacique <wbrand90@gmail.com>
2023-04-20 04:20:08 -0700

    Initial Commit

---
+++ SwiftUIAnimations/SwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsApp.swift
+//  SwiftUIAnimations
+//
+//  Created by William on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
